d20eb62c61b2fdc1f92691ddc9635263c2a13c57
mrhiu98 <nguyenxuanhieu271098@gmail.com>
2023-08-16 00:55:02 +0700

    fixbug2/list-admin-account

--- src/main/java/com/amand/constant/SystemConstant.java
+++ src/main/java/com/amand/constant/SystemConstant.java
@@ -9,4 +9,6 @@ public class SystemConstant {
     public static final int API_STATUS_OK = 1;
 
     public static final int API_STATUS_NG = 0;
+
+    public static final String ROLE_CODE = "ROLE_IMPLOY";
 }
--- src/main/java/com/amand/repository/UserRepository.java
+++ src/main/java/com/amand/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.amand.repository;
 
 import com.amand.entity.UserEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
@@ -12,4 +15,8 @@ public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
         @Query(value = "SELECT u.userName FROM UserEntity u WHERE u.userName = :username")
         String findOneByUserName(@Param("username") String userName);
+
+        @Query(value = "SELECT u FROM UserEntity u INNER JOIN u.roles r WHERE r.code = :roleCode")
+        List<UserEntity> findAllByRoleCode(@Param("roleCode") String roleCode, Pageable pageable);
+
 }
