3570d5c058c2dbe23a5e7a92be713337c52fa98e
mrhiu98 <nguyenxuanhieu271098@gmail.com>
2023-09-09 00:22:30 +0700

    feature/hide-size

--- src/main/java/com/amand/repository/SizeRepository.java
+++ src/main/java/com/amand/repository/SizeRepository.java
@@ -3,6 +3,7 @@ package com.amand.repository;
 import com.amand.entity.SizeEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -26,4 +27,8 @@ public interface SizeRepository extends JpaRepository<SizeEntity, Integer> {
 
     @Query(value = "SELECT count(s) FROM SizeEntity s WHERE s.status = :status")
     int countByStatus(@Param("status") Integer status);
+
+    @Modifying
+    @Query(value = "UPDATE SizeEntity s SET s.status = 0 WHERE s.id IN (:ids)")
+    void updateStatusByIds(@Param("ids") List<Integer> ids);
 }
